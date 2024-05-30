f75f25c733c94f9cef8056cc783cedaa681ef036
rocky000 <md.rockibulislamkhan@gmail.com>
2024-05-30 12:27:11 +0600

    modify the variable call method

--- 2-ec2.tf
+++ 2-ec2.tf
@@ -17,8 +17,8 @@ resource "aws_security_group" "ec2_sg" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami                    = "ami-0c55b159cbfafe1f0"  # Change to desired AMI
-  instance_type          = "t2.micro"  # Change to desired instance type
+  ami                    = var.ami
+  instance_type          = var.instance_type
   subnet_id              = aws_subnet.public_subnet.id
   associate_public_ip_address = true
   security_groups        = [aws_security_group.ec2_sg.id]
